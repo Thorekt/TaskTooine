37aa81c93343d76667248ac15ac92d967b34e18a
rory <35566455+Thorekt@users.noreply.github.com>
2026-04-02 23:38:17 +0200

    build: create java api project

---
+++ tasktooine-api/src/main/java/com/thorekt/tasktooine_api/TasktooineApiApplication.java
@@ -0,0 +1,13 @@
+package com.thorekt.tasktooine_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasktooineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasktooineApiApplication.class, args);
+	}
+
+}
---
+++ tasktooine-api/src/test/java/com/thorekt/tasktooine_api/TasktooineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thorekt.tasktooine_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktooineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
